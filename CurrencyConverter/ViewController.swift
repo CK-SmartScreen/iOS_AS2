fcd82ac969bd35dbcd80c3689cf716de14f8c5cc
Chun Kai <CK-SmartScreen@users.noreply.github.com>
2017-11-01 00:19:40 +1300

    finish basic UI except Currency List

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -5,48 +5,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var outputCurrency: UIButton!
     @IBOutlet weak var inputCountryButton: UIButton!
     @IBOutlet weak var outputCountryButton: UIButton!
+    @IBOutlet weak var DecimalPointButton: UIButton!
 
+
+    var inputString = "0"
+    var outputString = "0"
+    var operation1: Double = 0
+    var operation2: String = "="
     var exchangeRate: Double = 0.68
-    var inputCountryInfo: (String, String) = ("NZD", "$") {
+    var inputCurrencyInfo: (String, String) = ("NZD", "$") {
         willSet {
             inputCountryButton.setTitle(newValue.0, for: .normal)
         }
     }
-    var outputCountryInfo: (String, String) = ("USD", "$") {
+    var outputCurrencyInfo: (String, String) = ("USD", "$") {
         willSet {
             outputCountryButton.setTitle(newValue.0, for: .normal)
         }
     }
 
     var isFirstDigit = true
-    var inputValue: Double = 0
-    var outputValue: Double = 0
-    var inputString: String {
+
+    var inputValue: Double {
         get {
-            return tempString
+            return NumberFormatter().number(from: inputString)!.doubleValue
         }
         set {
-            inputCurrency.setTitle((inputCountryInfo.1 + newValue), for: .normal)
-            inputValue = Double (newValue)!
+            inputString = newValue == 0 ? "0" : String(format: "%.2f", newValue)
+            inputCurrency.setTitle(inputCurrencyInfo.1 + inputString, for: .normal)
+            isFirstDigit = true
+        }
+    }
+    var outputValue: Double {
+        get {
+            return NumberFormatter().number(from: outputString)!.doubleValue
+        }
+        set {
+            outputString = newValue == 0 ? "0" : String(format: "%.2f", newValue)
+            outputCurrency.setTitle(outputCurrencyInfo.1 + outputString, for: .normal)
         }
     }
 
 
-    var outputString: String {
-        get {
-            return String(format: "%.2f", outputValue)
-        }
-        set {
-            outputCurrency.setTitle((outputCountryInfo.1 + newValue), for: .normal)
-            outputValue = Double (newValue)!
-        }
-    }
-
-    var tempString = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        inputCountryButton.setTitle(inputCountryInfo.0, for: .normal)
-        outputCountryButton.setTitle(outputCountryInfo.0, for: .normal)
+        inputCountryButton.setTitle(inputCurrencyInfo.0, for: .normal)
+        outputCountryButton.setTitle(outputCurrencyInfo.0, for: .normal)
+        inputValue =  0
+        outputValue = 0
         inputString = "0"
         outputString = "0"
     }
@@ -56,42 +62,55 @@ class ViewController: UIViewController {
     }
     
     @IBAction func digitPress(_ sender: UIButton) {
-        let digit = sender.currentTitle!
-        tempString = isFirstDigit ? digit : tempString + digit
-        inputString = tempString
-        isFirstDigit = false
+        if (inputString.range(of:".") != nil && sender.currentTitle == ".") {
+            return
+        }
 
-        print(inputValue)
+        let digit = sender.currentTitle!
+        inputString = isFirstDigit ? digit : inputString + digit
+        inputCurrency.setTitle((inputCurrencyInfo.1 + inputString), for: .normal)
+
+        outputValue = inputValue * exchangeRate
+        outputString = String(format:"%.2f", inputValue * exchangeRate)
+
+        isFirstDigit = false
     }
 
-    @IBAction func ConvertAction(_ sender: Any) {
+    @IBAction func operationAction(_ sender: UIButton) {
+        operation2 = sender.currentTitle!
+        operation1 = inputValue
+        isFirstDigit = true
+        DecimalPointButton.isEnabled = true
+        DecimalPointButton.backgroundColor = UIColor.orange
+    }
+
+    @IBAction func calculateAction(_ sender: Any) {
+        switch operation2 {
+            case "+": inputValue += operation1
+            case "-": inputValue = operation1 - inputValue
+        default:
+            break
+        }
+
         outputValue = inputValue * exchangeRate
-        let c:String = String(format:"%.2f", outputValue)
-        outputCurrency.setTitle((outputCountryInfo.1 + c), for: .normal)
+        outputString = String(format:"%.2f", inputValue * exchangeRate)
     }
 
     @IBAction func deleteAction(_ sender: Any) {
+        inputValue = 0
+        outputValue = 0
         isFirstDigit = true
-        inputCurrency.setTitle("0", for: .normal)
-        outputCurrency.setTitle("0", for: .normal)
-        inputString = "0"
-        outputString = "0"
+
     }
 
     @IBAction func SwapAction(_ sender: Any) {
-        let tempTuple: (String, String) = inputCountryInfo
-        inputCountryInfo = outputCountryInfo
-        outputCountryInfo = tempTuple
-
-        let tempString = inputString
-        inputString = outputString
-        outputString = tempString
-
+        let tempTuple: (String, String) = inputCurrencyInfo
+        inputCurrencyInfo = outputCurrencyInfo
+        outputCurrencyInfo = tempTuple
+        let tmp = inputValue
+        inputValue = outputValue
+        outputValue = tmp
         exchangeRate = 1 / exchangeRate
     }
-
-    @IBAction func operationAction(_ sender: Any) {
-    }
-
 }
 
---
+++ README.md
@@ -0,0 +1,15 @@
+# Xcode:
++ V 9.0.1
++ Swift: 4.0
++ Support Device: 4s SE 8 8plus X
++ Target OS: iOS 11
+
+# USAGE
+```Swift
+var exchangeRate: Double = 0.68
+var inputCurrencyInfo: (String, String) = ("NZD", "$")
+var outputCurrencyInfo: (String, String) = ("USD", "$")
+```
++ You might need to retrieve a Currency list and create a table for user to choose, if you have time bro.
++ After user change either one or both currency, please update the above tuples and the exchangeRate.
++ Then the user can play this app for fun.
