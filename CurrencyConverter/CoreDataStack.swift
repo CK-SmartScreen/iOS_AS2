86e27f862a1d40173b573c94c65c62e0c4c460f7
Xavier Francis <thisisxvr@gmail.com>
2017-11-02 14:58:43 +1300

    Add CoreDataStack; Organize files

---
+++ CurrencyConverter/CoreDataStack.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataStack.swift
+//  Rates
+//
+//  Created by Xavier Francis on 31/10/17.
+//  Copyright © 2017 CM+XF. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+func createMainContext(completion: @escaping (NSPersistentContainer) -> Void) {
+    let container = NSPersistentContainer(name: "Rates")
+    print(container.persistentStoreDescriptions.first?.url! as Any) // Get SQLite DB location on disk.
+    
+    // Asynchronous
+    container.loadPersistentStores(completionHandler: {
+        persistentStoreDescription, error in
+        
+        guard error == nil else { fatalError("Failed to load store: \(String(describing: error))") }
+        
+        DispatchQueue.main.async {
+            completion(container)
+        }
+    })
+}
